c256619a75896d63fbc8e2e73d82c77a7fc10af5
Isaac Caro <36085104+IsaacMCaro@users.noreply.github.com>
2018-12-02 23:15:05 -0800

    Input File is Read and constructs a Dictionary with those values
    Added ability to read from file in server application. This puts the infromation from the file into a dictionary for updating and transmission.
    
    Function added to Client  that determines whether it will bid on an item (30% chance) and client now sends string to server as a test.

--- Server.py
+++ Server.py
@@ -6,12 +6,25 @@ port = 12345
 
 s.bind(('', port))
 print("Socket binded to %s" %(port))
-
 s.listen(5)
 print("Socket is listening!")
 
+#Block for Data Collection from file
+f = open("input.txt", "r")
+flines = f.readlines()
+itemData = {}
+for i in range(1,len(flines)):
+    nline = flines[i].split()
+    itemData[nline[0]] = [int(nline[1]), int(nline[2])] #Puts data in dictionary organized by {"Item Name":[Units,Price]} for updating and reading
+print(itemData)
+f.close()
+
+#Transmitting Block
 while True:
     c, addr = s.accept()
     print("Got connection from", addr)
     c.send(str.encode('Thank you for connecting!'))
+    s.listen(5)
+    print("It looks like were receiving a message, It says: ")
+    print((c.recv(1024)).decode('utf-8'))
     c.close()
--- client.py
+++ client.py
@@ -1,5 +1,11 @@
 import socket
 import sys
+import random
+
+#Function that returns a Bool to determine whether to bid on item or not
+def probBid():
+    chance = random.randint(0,100)
+    return(chance >= 0 and chance <= 33)
 
 s = socket.socket()
 
@@ -8,5 +14,6 @@ port = 12345
 s.connect(('127.0.0.1', port))
 
 print((s.recv(1024)).decode('utf-8'))
+s.send(str.encode('Hope all is well!'))
 
 s.close()
