0cc973beef0434d5bf8b45a6f6a4a68c4c5f3b65
Isaac Caro <isaaccaro24@mail.fresnostate.edu>
2018-12-08 14:41:08 -0800

    Added most if not all of the necessary functions

--- Server.py
+++ Server.py
@@ -1,30 +1,149 @@
 import socket
+import pickle
+import random
+'''
+TO DO:
+- fix an syntax errors
+-organize code
+- get clients and server to connect
+
+'''
+
+bidTracker = {} #Dictionary to keep track of the bidding process Stored in {addr: Item Name} Format
+clients = [] #Array to keep track of client information (socket obj, addr)
+multiBid = {} #Dictionary that keeps track of items that are being bid on by multiple clinets {Item Name: [addr]}
+itemData = {} #Stores item information taken from input file {ItemName: (units, max bid)}
+winnerInfo = {} #stores record of items won {addr: [(itemName, amount owned)]}
+
+##FUNCTIONS##
+def getSocket(addr):
+    global clients
+    for i in range (0,4):
+        if (clients[i][1]== addr):
+            return clients[i][0]
+
+def itemWon(itemName, addr):
+    win = "Server: Congratulations! You've won 1 " + itemName + ". "
+    c = None
+    global winnerInfo
+    global itemData
+    for i in range(0,4):
+        if (client[i][1]==addr):
+            c = client[i][0]
+    c.send(str.encode("Won"))
+    c.send(str.encode(win))
+    if addr not in winnerInfo:
+        winnerInfo[addr] = [(itemName, 1)]
+    if addr in winnerInfor:
+        list = winnerInfo[addr]
+        for i in range(len(list)):
+            if itemName in list[i]:
+                list[i][1]+=1
+        list.append((itemName, 1))
+    itemData[itemName][0] -= 1
+
+def appendValue(dict, key, val):
+    if key in dict:
+        list= dict[key]
+        if val not in list:
+            dict[key].append(val)
+    dict[key]= [val]
+
+#Asks array of clients for their bids
+def solicitBids():
+    invite = "Server: You may now bid on an item. "
+    global bidTracker
+    global clients
+    for i in range (0,len(clients) - 1):
+        c = clients[i][0]
+        c.send(str.encode(invite))
+        temp = c.recv(recv(1024)).decode('utf-8') #Will Recv string Item Name and added into bids array
+        bidTracker[addr] = temp
+
+#Checks for case where one item was bid on by more than 1 client, returns dict of items: [addr]
+def checkMultBid():
+    global multiBid
+    global bidTracker
+    for key in bidTracker:
+        copy = bidTracker
+        tempTup = bidTracker[key]
+        del dictCopy[key]
+        for k in dictCopy:
+            if (dict[key][0] == dictCopy[k][0]):
+                itemName = dict[key][0]
+                appendValue(multiBid, itemName, key) #puts item&addr into conflicting bid dict
+
+
+def bidWarMode(itemName, listAddr):
+    msg1="Server: Other clients have bid on " + itemName + "."
+    itemWon(itemName, bidWarH(itemName, listAddr, 50, None))
+
+
+#recursive call that keeps bid war going, returns addr of person who won
+def bidWarH(itemName, listAddr, price, leader):
+    global clients
+    newPrice = price+1
+    losing = "Server: You have been outbid by another client, submit a new bid or lose the item. The price to beat is: $" + str(price+1) + ". "
+    winning = "Server: You are currently winning the bid with a price of $" + str(newPrice) + ". "
+    lost = "Server: I'm sorry but you did not win " + itemName + ". "
+    if (newPrice >= itemData[itemName][1]):
+        if clients[i][0] in listAddr:
+                clients[i][0].send(str.encode(lost))
+        return leader
+    nleader = listAddr[random.randint(0,len(listAddr)-1)]
+    listAddr.remove(nleader)
+    if (leader != None):
+        listAddr.append(leader)
+    newBids = []
+    for i in range(0, len(clients)-1):
+        if (clients[i][1] == nleader):
+            clients[i][0].send(str.encode("Winning"))
+            clients[i][0].send(str.encode(winning))
+        elif clients[i][0] in listAddr:
+                clients[i][0].send(str.encode("Outbid"))
+                clients[i][0].send(str.encode(losing))
+                clients[i][0].send(pickle.dumps(newPrice))
+                clients[i][0].settimeout(5.0)
+                if (pickle.loads(clients[i][0].rcv(1024)) == newPrice+1):
+                    newBids.append(clients[i][1])
+    if (newBids!= []):
+        bidWarH(itemName, newBids, newPrice, nleader)
+    return nleader
+
+
+
 s = socket.socket()
-print("Socket successfully created!")
-
 port = 12345
-
-s.bind(('', port))
-print("Socket binded to %s" %(port))
+s.bind(('127.0.0.1', port))
+print("Listening for Clients")
 s.listen(5)
-print("Socket is listening!")
+
+#intializes array of clients
+for i in range(0,4):
+    c, addr = s.accept()
+    clients[i] = (c,addr)
 
 #Block for Data Collection from file
 f = open("input.txt", "r")
 flines = f.readlines()
-itemData = {}
 for i in range(1,len(flines)):
     nline = flines[i].split()
     itemData[nline[0]] = [int(nline[1]), int(nline[2])] #Puts data in dictionary organized by {"Item Name":[Units,Price]} for updating and reading
 print(itemData)
 f.close()
 
-#Transmitting Block
+#loop until input, once ended transmit all items in winnerInfo
 while True:
-    c, addr = s.accept()
-    print("Got connection from", addr)
-    c.send(str.encode('Thank you for connecting!'))
-    s.listen(5)
-    print("It looks like were receiving a message, It says: ")
-    print((c.recv(1024)).decode('utf-8'))
-    c.close()
+    c.send(pickle.dumps(itemData))
+    solicitBids()
+    checkMultBid()
+    for key in bidTracker:
+        if key not in multibid:
+            c = getSocket(key)
+            c.send(str.encode("Default"))
+            c.send(str.encode("Server: No one else has bid on your item."))
+            itemWon(bidTacker[key],key)
+    for key in multiBid:
+        bidWarMode(key, multiBid[key])
+    multiBid.clear()
+    bidTracker.clear()
--- client.py
+++ client.py
@@ -1,19 +1,45 @@
 import socket
 import sys
 import random
+import pickle
+
+willBid = "We will bid on "
 
 #Function that returns a Bool to determine whether to bid on item or not
 def probBid():
     chance = random.randint(0,100)
     return(chance >= 0 and chance <= 33)
+#Accepts a dictionary and randomly selects an item to bid
+def chooseItem(dict):
+    return dict.keys()[random.randint(0,9)] #returns random item name to be bid on
 
 s = socket.socket()
-
 port = 12345
-
 s.connect(('127.0.0.1', port))
 
-print((s.recv(1024)).decode('utf-8'))
-s.send(str.encode('Hope all is well!'))
+while True:
+    itemData = pickle.loads(s.recv(1024))
+    itemName = chooseItem(itemData)
+    print(s.recv(recv(1024)).decode('utf-8'))
+    print(willBid + itemName+ ". ")
+    s.send(str.encode(itemName))
+    l = True
+    while (l):
+        choice = (s.recv(1024)).decode('utf-8')
+        if (choice == "Outbid"):
+            print((s.recv(1024)).decode('utf-8'))
+            newPrice = pickle.loads(s.recv(1024)) + 1
+            if (probBid()):
+                s.send(pickle.dumps(newPrice))
+            if (not probBid()):
+                    l = False
+        elif(choice == "Winning"):
+            print((s.recv(1024)).decode('utf-8'))
+        elif(choice == "Won"):
+            print((s.recv(1024)).decode('utf-8'))
+            l = False
+        elif(choice == "Default"):
+            print((s.recv(1024)).decode('utf-8'))
+        else: l = False
 
 s.close()
